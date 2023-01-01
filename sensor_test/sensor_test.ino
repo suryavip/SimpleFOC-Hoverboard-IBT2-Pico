ced5e710c9dfe325ef1c355c7671d377aebcbebf
suryavip <suryabejibun@gmail.com>
2023-01-01 22:48:27 +0700

    rename variable to match simpleFOC's naming

--- sensor_test/sensor_test.ino
+++ sensor_test/sensor_test.ino
@@ -1,29 +1,27 @@
 #include <SimpleFOC.h>
 
-pin_size_t ENCODER_SDA_PIN = 0;
-pin_size_t ENCODER_SCL_PIN = 1;
+// Wiring
+pin_size_t MAGNETIC_SENSOR_SDA_PIN = 0;
+pin_size_t MAGNETIC_SENSOR_SCL_PIN = 1;
 
-const uint8_t ENCODER_ADDRESS = 0x36;
-const uint8_t ENCODER_RESOLUTION = 12;
-const uint8_t ENCODER_ANGLE_REGISTER_MSB = 0x0C;  // or 0x0E (appear to have deadzone)
-const uint8_t ENCODER_BITS_USED_MSB = 4;
+const uint8_t MAGNETIC_SENSOR_ADDRESS = 0x36;
+const uint8_t MAGNETIC_SENSOR_RESOLUTION = 12;
+const uint8_t MAGNETIC_SENSOR_ANGLE_REGISTER_MSB = 0x0C;  // or 0x0E (appear to have deadzone)
+const uint8_t MAGNETIC_SENSOR_BITS_USED_MSB = 4;
 
 MagneticSensorI2C sensor = MagneticSensorI2C(
-  ENCODER_ADDRESS,
-  ENCODER_RESOLUTION,
-  ENCODER_ANGLE_REGISTER_MSB,
-  ENCODER_BITS_USED_MSB);
+  MAGNETIC_SENSOR_ADDRESS,
+  MAGNETIC_SENSOR_RESOLUTION,
+  MAGNETIC_SENSOR_ANGLE_REGISTER_MSB,
+  MAGNETIC_SENSOR_BITS_USED_MSB);
 
 
 void setup() {
-  // monitoring port
   Serial.begin(115200);
 
-  Wire.setSDA(ENCODER_SDA_PIN);
-  Wire.setSCL(ENCODER_SCL_PIN);
-  // configure i2C
-  // Wire.setClock(400000);
-  // initialise magnetic sensor hardware
+  Wire.setSDA(MAGNETIC_SENSOR_SDA_PIN);
+  Wire.setSCL(MAGNETIC_SENSOR_SCL_PIN);
+  Wire.setClock(400000);
   sensor.init();
 
   Serial.println("Sensor ready");
